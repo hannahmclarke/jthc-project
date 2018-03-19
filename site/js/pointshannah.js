294cf7fff632907f90a53593e43b56185caa87c5
hannahmclarke <35969272+hannahmclarke@users.noreply.github.com>
2018-03-19 23:31:13 +0000

    Fixed responsiveness etc
    Social media buttons fixed,
    Featured routes now responsive to smaller browser sizes,
    Featured Trippers is parallax scrolling. AWESOME

--- site/js/pointshannah.js
+++ site/js/pointshannah.js
@@ -221,7 +221,7 @@ new mapboxgl.Marker(el, {
   offset: [0, -25]
 })
 .setLngLat(marker.geometry.coordinates)
-.setPopup(new mapboxgl.Popup({ offset: 25 }) // add popups
+.setPopup(new mapboxgl.Popup({ offset: 50 }) // add popups
 .setHTML('<h3>' + marker.properties.title + '</h3><p>' + marker.properties.description + '</p>'))
 .addTo(map);
 });
